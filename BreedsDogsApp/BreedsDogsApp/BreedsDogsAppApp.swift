5a9b885bff36a986fdd409863eb32cc2fcaa8d00
David Jesus Maya Quiros <dmaya.3@outlook.com>
2025-04-11 19:02:55 +0200

    I´ve created the project

---
+++ BreedsDogsApp/BreedsDogsApp/BreedsDogsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreedsDogsAppApp.swift
+//  BreedsDogsApp
+//
+//  Created by David Jesús Maya Quirós on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BreedsDogsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreedsDogsApp/BreedsDogsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BreedsDogsApp
+//
+//  Created by David Jesús Maya Quirós on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
